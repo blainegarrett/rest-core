8c8d810b258108e2ddca70c5ae8cb289452cef18
Blaine F Garrett <blaine@blainegarrett.com>
2016-07-11 18:03:21 -0500

    Moving example app back to the example directory.

---
+++ example/main.py
@@ -0,0 +1,41 @@
+""" Example Application """
+
+import os
+import sys
+import webapp2
+import json
+
+# Bootstrap the external libs
+sys.path.insert(0, '/Users/blainegarrett/sites/rest-core')
+sys.path.insert(0, '/Users/blainegarrett/sites/rest-core/external')
+
+#raise Exception(sys.path)
+
+import voluptuous
+from rest_core import handlers
+
+
+def serve_response(response, status, result, messages=None):
+    """
+    Serve a response
+    """
+
+    payload = {'status': status, 'results': result, 'messages': messages}
+    response.set_status(status)
+    response.headers['content-type'] = 'application/json'
+    response.write(json.dumps(payload))
+
+
+def handle_404(request, response, exception):
+    """
+    Top Level Route handler for 404
+    """
+
+    err = 'Rest endpoint not found or unavailable'
+
+    serve_response(response, 404, None, messages=[err])
+
+
+routes = []
+app = webapp2.WSGIApplication(routes, debug=True)
+app.error_handlers[404] = handle_404
